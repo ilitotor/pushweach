8bbf65253399b9f524b7df6ef15f53e4727c45ed
Ilito Torquato <ilitotor@gmail.com>
2019-04-03 14:16:32 +0000

    fixing settings.py errors

--- push/settings.py
+++ push/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'app.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
